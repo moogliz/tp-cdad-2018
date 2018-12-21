89f428c553114628d15a3deb951f9e38f6f4caca
Soulou <leo@unbekandt.eu>
2018-12-21 16:17:05 +0100

    return promises

--- migrations/20181221161252_add_timestamps_to_sentences.js
+++ migrations/20181221161252_add_timestamps_to_sentences.js
@@ -1,12 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  knex.schema.table("sentences", function(table) {
-    table.timestamps();
+  return knex.schema.table("sentences", function(table) {
+    table.timestamp('createdAt')
+    table.timestamp('updatedAt')
   })
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.table("sentences", function(table) {
-    table.dropTimestamps();
+  return knex.schema.table("sentences", function(table) {
+    table.dropColumns(['createdAt', 'updatedAt']);
   })
 };
