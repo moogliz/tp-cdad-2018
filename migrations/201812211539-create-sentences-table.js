05f96553be4a56a228e9b0b84b3d6e30484050d2
Soulou <leo@unbekandt.eu>
2018-12-21 16:04:11 +0100

    Return promise

--- migrations/201812211539-create-sentences-table.js
+++ migrations/201812211539-create-sentences-table.js
@@ -1,12 +1,12 @@
 exports.up = function(knex, Promise) {
-  knex.schema.createTable('sentences', function (table) {
+  return knex.schema.createTable('sentences', function (table) {
     table.increments();
     table.string('sentence');
   });
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.dropTable('sentences');
+  return knex.schema.dropTable('sentences');
 };
 
 exports.config = { transaction: false };
