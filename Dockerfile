46c76aaf78302c315baf6e115edd969f692c0148
Soulou <leo@unbekandt.eu>
2018-12-21 15:48:04 +0100

    Use knex to apply migrations

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:10
 
-RUN npm install -g sails nodemon
+RUN npm install -g sails nodemon knex
 
 WORKDIR /app
 EXPOSE 1337
--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-postgresql',
-    url: process.env['POSTGRESQL_URL'],
+    url: process.env.DATABASE_URL,
   },
 
 
